d1b8fd3b393fc38283653ac46d6f2f3ce703e3ff
Pranav Bakale <pranav.bakale@gmail.com>
2024-09-26 14:03:54 +0530

    fix: removed default values from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Widget from "./components/Widget";
 function App() {
   return (
     <div className="flex justify-center w-full">
-      <Widget projectId={"1"} loop={true} autoplay={true} delay={2000} />
+      <Widget />
     </div>
   );
 }
